1f4ddfb1e2d37ba08fc0d97becdc3c71d5ab6520
Ronda D Cowen <cowenhome5@gmail.com>
2020-07-02 22:00:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-Rails.application.routes.draw do get 'about', to: 'static#about
+Rails.application.routes.draw do get 'about', to: 'static#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
